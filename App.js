4d51101513e5dd1aab4ad8347f236b109a710818
panji31 <gumilarp@gmail.com>
2019-10-06 15:30:43 +0700

    add 2.add_todo

--- App.js
+++ App.js
@@ -1,114 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Container, Header, Content, List, ListItem, Text, InputGroup, Input, Button } from 'native-base';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      field: "",
+      data: [],
+    };
+  }
+  setText = (text) => {
+    this.setState({
+      field: text
+    })
+  }
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+  insertText = () => {
+    var kata = this.state.field;
+    var data_kata = this.state.data;
+    data_kata.push(kata);
+    this.setState({
+      data: data_kata,
+      field: "",
+    });
+  }
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+          <List>
+            <ListItem >
+              <InputGroup borderType="regular" >
+                <Input value={this.state.field} onChangeText={this.setText} placeholder="Type your text here" />
+                <Button onPress={this.insertText}><Text>ADD</Text></Button>
+              </InputGroup>
+            </ListItem>
+            {this.state.data.map((item, index) => {
+              return (
+                <ListItem>
+                  <Text>{item}</Text>
+                </ListItem>)
+            })}
+          </List>
+        </Content>
+      </Container >
+    );
+  }
+}
--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import Makelist from './Makelist';
+import Makelist2 from './Makelist2';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Makelist);
+AppRegistry.registerComponent(appName, () => Makelist2); 
