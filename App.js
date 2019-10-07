fd34d59545fc7d4a269a56bf9ff6eac999434b01
panji31 <gumilarp@gmail.com>
2019-10-07 22:38:15 +0700

    add 2.add_todo

--- App.js
+++ App.js
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, List, ListItem, Text, InputGroup, Input, Button } from 'native-base';
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      field: "",
-      data: [],
-    };
-  }
-  setText = (text) => {
-    this.setState({
-      field: text
-    })
-  }
+    constructor() {
+        super();
+        this.state = {
+            field: "",
+            data: [],
+        };
+    }
+    setText = (text) => {
+        this.setState({
+            field: text
+        })
+    }
 
-  insertText = () => {
-    var kata = this.state.field;
-    var data_kata = this.state.data;
-    data_kata.push(kata);
-    this.setState({
-      data: data_kata,
-      field: "",
-    });
-  }
-  render() {
-    return (
-      <Container>
-        <Header />
-        <Content>
+    insertText = () => {
+        var kata = this.state.field;
+        var data_kata = this.state.data;
+        data_kata.push(kata);
+        this.setState({
+            data: data_kata,
+            field: "",
+        });
+    }
+    render() {
+        return (
+            <Container>
+                <Header />
+                <Content>
 
-          <List>
-            <ListItem >
-              <InputGroup borderType="regular" >
-                <Input value={this.state.field} onChangeText={this.setText} placeholder="Type your text here" />
-                <Button onPress={this.insertText}><Text>ADD</Text></Button>
-              </InputGroup>
-            </ListItem>
-            {this.state.data.map((item, index) => {
-              return (
-                <ListItem>
-                  <Text>{item}</Text>
-                </ListItem>)
-            })}
-          </List>
-        </Content>
-      </Container >
-    );
-  }
+                    <List>
+                        <ListItem >
+                            <InputGroup borderType="regular" >
+                                <Input value={this.state.field} onChangeText={this.setText} placeholder="Type your text here" />
+                                <Button onPress={this.insertText}><Text>ADD</Text></Button>
+                            </InputGroup>
+                        </ListItem>
+                        {this.state.data.map((item, index) => {
+                            return (
+                                <ListItem>
+                                    <Text>{item}</Text>
+                                </ListItem>)
+                        })}
+                    </List>
+                </Content>
+            </Container >
+        );
+    }
 }
