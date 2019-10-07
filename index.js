58c53bb907ea8d2e8a6f7f22e32b2efdc4d2c58a
panji31 <gumilarp@gmail.com>
2019-10-07 22:31:05 +0700

    add make_list

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import Makelist from './Makelist';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Makelist);
+AppRegistry.registerComponent(appName, () => App);
