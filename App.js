37c39dcb258e4105124f3c5346696dec1046a3db
panji31 <gumilarp@gmail.com>
2019-10-07 22:52:48 +0700

    add 4.todo_is_done

--- App.js
+++ App.js
@@ -1,97 +1,100 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, List, ListItem, Text, InputGroup, Input, Button, Icon, Left, Body, Right, CheckBox } from 'native-base';
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      field: "",
-      data: [],
-    };
-  }
-  setText = (text) => {
-    this.setState({
-      field: text
-    })
-  }
-
-  insertText = () => {
-    const kata = this.state.field;
-    const data_kata = this.state.data;
-    const objek = {};
-    objek.name = kata;
-    objek.status = false;
-    data_kata.push(objek);
-
-    this.setState({
-      data: data_kata,
-      field: "",
-    });
-  }
-
-  deleteText = (name) => {
-    const pilihan = this.search(name, this.state.data);
-    this.state.data.splice(pilihan, 1);
-    this.setState({
-      data: this.state.data,
-    });
-  }
-
-  updateText = (name) => {
-
-    const pilihan = this.search(name, this.state.data);
-    const aray = this.state.data;
-
-    if (aray[pilihan].status == true) {
-      aray[pilihan].status = false;
-    } else {
-      aray[pilihan].status = true;
+    constructor() {
+        super();
+        this.state = {
+            field: "",
+            data: [],
+        };
+        this.hitung = 0;
+    }
+    setText = (text) => {
+        this.setState({
+            field: text
+        })
     }
 
-    this.setState({
-      data: aray,
-    });
+    insertText = () => {
+        const kata = this.state.field;
+        const data_kata = this.state.data;
+        const objek = {};
+        this.hitung += 1;
+        objek.id = this.hitung;
+        objek.name = kata;
+        objek.status = false;
+        data_kata.push(objek);
 
-  }
-
-  search(nameKey, myArray) {
-    for (let i = 0; i < myArray.length; i++) {
-      if (myArray[i].name === nameKey) {
-        return i;
-      }
+        this.setState({
+            data: data_kata,
+            field: "",
+        });
     }
-  }
 
-  render() {
-    return (
-      <Container>
-        <Header />
-        <Content>
+    deleteText = (id) => {
+        const pilihan = this.search(id, this.state.data);
+        this.state.data.splice(pilihan, 1);
+        this.setState({
+            data: this.state.data,
+        });
+    }
 
-          <List>
-            <ListItem >
-              <InputGroup borderType="regular" >
-                <Input value={this.state.field} onChangeText={this.setText} placeholder="Type your text here" />
-                <Button onPress={this.insertText}><Text>ADD</Text></Button>
-              </InputGroup>
-            </ListItem>
-            {this.state.data.map((item, index) => {
-              return (
-                <ListItem icon>
-                  <CheckBox onPress={this.updateText.bind(this, item.name)} checked={item.status} />
-                  <Body>
-                    <Text>{item.name}</Text>
-                  </Body>
-                  <Right>
-                    <Button onPress={this.deleteText.bind(this, item.name)} style={{ backgroundColor: 'grey' }} >
-                      <Icon active name="trash" style={{ color: 'red' }} />
-                    </Button>
-                  </Right>
-                </ListItem>
-              )
-            })}
-          </List>
-        </Content>
-      </Container >
-    );
-  }
+    updateText = (id) => {
+
+        const pilihan = this.search(id, this.state.data);
+        const aray = this.state.data;
+
+        if (aray[pilihan].status == true) {
+            aray[pilihan].status = false;
+        } else {
+            aray[pilihan].status = true;
+        }
+
+        this.setState({
+            data: aray,
+        });
+
+    }
+
+    search(nameKey, myArray) {
+        for (let i = 0; i < myArray.length; i++) {
+            if (myArray[i].id === nameKey) {
+                return i;
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <Header />
+                <Content>
+
+                    <List>
+                        <ListItem >
+                            <InputGroup borderType="regular" >
+                                <Input value={this.state.field} onChangeText={this.setText} placeholder="Type your text here" />
+                                <Button onPress={this.insertText}><Text>ADD</Text></Button>
+                            </InputGroup>
+                        </ListItem>
+                        {this.state.data.map((item, index) => {
+                            return (
+                                <ListItem icon>
+                                    <CheckBox onPress={this.updateText.bind(this, item.id)} checked={item.status} />
+                                    <Body>
+                                        <Text>{item.name}</Text>
+                                    </Body>
+                                    <Right>
+                                        <Button onPress={this.deleteText.bind(this, item.id)} style={{ backgroundColor: 'grey' }} >
+                                            <Icon active name="trash" style={{ color: 'red' }} />
+                                        </Button>
+                                    </Right>
+                                </ListItem>
+                            )
+                        })}
+                    </List>
+                </Content>
+            </Container >
+        );
+    }
 }
--- index.js
+++ index.js
@@ -3,11 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
-import Makelist from './Makelist';
-import Makelist2 from './Makelist2';
-import Makelist3 from './Makelist3';
-import Makelist4 from './Makelist4';
+import App from './App'; 
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Makelist4); 
+AppRegistry.registerComponent(appName, () => App);  
