c18934ae159dfc6ad9dcc8fef0669df6419e3a94
panji31 <gumilarp@gmail.com>
2019-10-08 00:48:47 +0700

    add 3.delete_todo

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, List, ListItem, Text, InputGroup, Input, Button, Icon, Left, Body, Right, CheckBox } from 'native-base';
+import { Container, Header, Content, List, ListItem, Text, InputGroup, Input, Button, Icon, Body, Right } from 'native-base';
 export default class App extends Component {
     constructor() {
         super();
@@ -7,38 +7,34 @@ export default class App extends Component {
             field: "",
             data: []
         };
-        this.hitung = 0;
+        this.count = 0;
     }
     setText = (text) => {
         this.setState({
             field: text
         })
     }
-
     insertText = () => {
-        const kata = this.state.field;
-        const data_kata = this.state.data;
-        const objek = {};
-        this.hitung += 1;
-        objek.id = this.hitung;
-        objek.name = kata;
-        objek.status = false;
-        data_kata.push(objek);
-
+        const state_field = this.state.field;
+        const state_data = this.state.data;
+        const object = {};
+        this.count += 1;
+        object.id = this.count;
+        object.name = state_field;
+        object.status = false;
+        state_data.push(object);
         this.setState({
-            data: data_kata,
+            data: state_data,
             field: "",
         });
     }
-
     deleteText = (id) => {
-        const pilihan = this.search(id, this.state.data);
-        this.state.data.splice(pilihan, 1);
+        const index = this.search(id, this.state.data);
+        this.state.data.splice(index, 1);
         this.setState({
             data: this.state.data,
         });
     }
-
     search(nameKey, myArray) {
         for (let i = 0; i < myArray.length; i++) {
             if (myArray[i].id === nameKey) {
@@ -46,7 +42,6 @@ export default class App extends Component {
             }
         }
     }
-
     render() {
         return (
             <Container>
@@ -67,7 +62,7 @@ export default class App extends Component {
                                         <Text>{item.name}</Text>
                                     </Body>
                                     <Right>
-                                        <Button onPress={this.deleteText.bind(this, item.id)} style={{ backgroundColor: 'grey' }} >
+                                        <Button onPress={this.deleteText.bind(this, item.id)} style={{ backgroundColor: 'white', borderTopWidth: 0.5, borderBottomWidth: 1, borderColor: '#d6d7da', }} >
                                             <Icon active name="trash" style={{ color: 'red' }} />
                                         </Button>
                                     </Right>
@@ -79,4 +74,4 @@ export default class App extends Component {
             </Container >
         );
     }
-} 
+}
